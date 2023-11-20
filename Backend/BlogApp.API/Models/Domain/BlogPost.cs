9807a8b19c545a6951592bdde23aca8527d6056f
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-20 17:50:17 +0100

    Initial Commit

---
+++ Backend/BlogApp.API/Models/Domain/BlogPost.cs
@@ -0,0 +1,14 @@
+﻿namespace BlogApp.API.Models.Domain;
+
+public class BlogPost
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string ShortDescription { get; set; }
+    public string Content { get; set; }
+    public string FeaturedImageUrl { get; set; }
+    public string UrlHandle { get; set; }
+    public DateTime PublicationDate { get; set; }
+    public string Author { get; set; }
+    public bool IsVisible { get; set; }
+}
---
+++ Backend/BlogApp.API/Models/Domain/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace BlogApp.API.Models.Domain;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string UrlHandle { get; set; }
+}
