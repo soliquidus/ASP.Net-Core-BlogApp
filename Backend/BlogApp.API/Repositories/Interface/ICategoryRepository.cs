de82e039d8d400ec9ba97b993bc8f805b848af10
Tibo Pfeifer <tibo.pfeifer@cgi.com>
2023-11-22 16:55:55 +0100

    Feat: Categories CRUD

--- Backend/BlogApp.API/Repositories/Interface/ICategoryRepository.cs
+++ Backend/BlogApp.API/Repositories/Interface/ICategoryRepository.cs
@@ -6,4 +6,7 @@ public interface ICategoryRepository
 {
     Task<Category> CreateAsync(Category category);
     Task<IEnumerable<Category>> GetAllAsync();
+    Task<Category?> GetById(Guid id);
+    Task<Category?> UpdateAsync(Category category);
+    Task<Category?> DeleteAsync(Guid id);
 }
